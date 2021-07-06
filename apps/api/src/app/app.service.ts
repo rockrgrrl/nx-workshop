f2c429dfa9b79ead953f0ae955a8eccdd3d64dd4
Gina Krieb <ginakrieb@Ginas-MacBook-Pro.local>
2021-07-06 15:01:29 -0500

    Mid lab 9

--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
+import { Game } from '@bg-hoard/api/util-interface';
 
-const games = [
+
+const games: Game[] = [
   {
     id: 'settlers-in-the-can',
     name: 'Settlers in the Can',
